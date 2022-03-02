9bf8aed716ee88ec90a0a8e3d82c262fa2723b12
eduardo saldana <edusal100@hotmail.com>
2022-03-01 23:28:19 -0600

    added mobile menu overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
     
     if(header.classList.contains('open')){ //close hamburger menu
         header.classList.remove('open');
+        overlay.classList.remove('view');
 
     }
-    else { // close hamburger menu
+    else { // open hamburger menu
         header.classList.add('open');
+        overlay.classList.add('view');
 
     }
 });
